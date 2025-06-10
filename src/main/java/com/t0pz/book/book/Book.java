a189073b127b736786b86d54d2c9a934df986f24
t0pZ <kristoffer.laiz@outlook.com>
2025-06-10 15:02:20 +0800

    Initialize BaseEntity and refactor Book and Feedback entities to use BaseEntity entity

--- src/main/java/com/t0pz/book/book/Book.java
+++ src/main/java/com/t0pz/book/book/Book.java
@@ -1,22 +1,20 @@
 package com.t0pz.book.book;
 
+import com.t0pz.book.common.BaseEntity;
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
-
-    @Id
-    @GeneratedValue
-    private Integer id;
-
+public class Book extends BaseEntity {
     private String title;
 
     private String authorName;
@@ -30,20 +28,4 @@ public class Book {
     private boolean archived;
 
     private boolean shareable;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }
