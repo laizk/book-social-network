7be245e385ca9f03918e5980987ff71edc586342
t0pZ <kristoffer.laiz@outlook.com>
2025-06-10 13:42:08 +0800

    Comment out @Transactional annotation from activateAccount as it prevents registering the token upon activation of account

--- src/main/java/com/t0pz/book/auth/AuthenticationService.java
+++ src/main/java/com/t0pz/book/auth/AuthenticationService.java
@@ -113,7 +113,7 @@ public class AuthenticationService {
                 .build();
     }
 
-    @Transactional
+    //@Transactional
     public void activateAccount(String token) throws MessagingException {
         Token savedToken = tokenRepository.findByToken(token)
                 // todo exception has to be defined
