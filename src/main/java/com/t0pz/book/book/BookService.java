73b28f87fcef3326ac128c8e0d9d9ab08ed6efc9
t0pZ <kristoffer.laiz@outlook.com>
2025-06-18 00:23:16 +0800

    Implement BookController methods: save and findBookById

---
+++ src/main/java/com/t0pz/book/book/BookService.java
@@ -0,0 +1,29 @@
+package com.t0pz.book.book;
+
+import com.t0pz.book.user.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    public Integer save(BookRequest request, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Book book = bookMapper.toBook(request);
+        book.setOwner(user);
+        return bookRepository.save(book).getId();
+    }
+
+    public BookResponse findById(Integer bookId) {
+        return bookRepository.findById(bookId)
+                .map(bookMapper::toBookResponse)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with the ID:: " + bookId))
+                ;
+    }
+}
