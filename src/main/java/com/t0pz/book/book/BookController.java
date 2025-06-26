5e8553fcaa93fa1bf1bc59f358648e0f07c3313c
t0pZ <kristoffer.laiz@outlook.com>
2025-06-26 10:04:44 +0800

    Implement findAllBooks controller

--- src/main/java/com/t0pz/book/book/BookController.java
+++ src/main/java/com/t0pz/book/book/BookController.java
@@ -1,5 +1,6 @@
 package com.t0pz.book.book;
 
+import com.t0pz.book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,5 +29,14 @@ public class BookController {
             @PathVariable("book-id") Integer bookId
     ){
         return ResponseEntity.ok(service.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
 }
--- src/main/java/com/t0pz/book/book/BookService.java
+++ src/main/java/com/t0pz/book/book/BookService.java
@@ -1,10 +1,17 @@
 package com.t0pz.book.book;
 
+import com.t0pz.book.common.PageResponse;
 import com.t0pz.book.user.User;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +32,23 @@ public class BookService {
                 .map(bookMapper::toBookResponse)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with the ID:: " + bookId))
                 ;
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        Page<Book> books = bookRepository.findAllDisplayableBooks(pageable, user.getId());
+        List<BookResponse> bookResponses = books.stream()
+                .map(bookMapper::toBookResponse)
+                .toList();
+        return new PageResponse<>(
+                bookResponses,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
